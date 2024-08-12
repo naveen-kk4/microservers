d04b0aea9c3eba1c0f975e2e2644c61ef0b55703
naveen-kk4 <naveennig2001@gmail.com>
2024-08-12 19:40:24 +0530

    first commit

---
+++ accounts/src/main/java/com/microservices/accounts/Controller/AccountsController.java
@@ -0,0 +1,14 @@
+package com.microservices.accounts.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountsController {
+
+    @GetMapping("/sayMyName")
+    public String sayMyName(){
+        return "Hello Heisenberg!";
+    }
+}
